35c53555a7b29299feac82d36866752f162ebacc
Donovan Griego <Xylobyte@users.noreply.github.com>
2020-02-11 20:26:52 -0700

    Update 2/11/2020

---
+++ Assets/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    bool gameHaseEnded = false;
+    public float restartDelay = 2f;
+
+    public void EndGame()
+    {
+        if(gameHaseEnded == false)
+        {
+            gameHaseEnded = true;
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,55 @@
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    private bool moveRight, moveLeft;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello, World!");
+        
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (moveRight)
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (moveLeft)
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey("d"))
+        {
+            moveRight = true;
+        } else
+        {
+            moveRight = false;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            moveLeft = true;
+        }
+        else
+        {
+            moveLeft = false;
+        }
     }
 }
